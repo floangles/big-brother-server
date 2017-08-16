3a53980da4b00b72c0b8d6c5076a236d405db859
floangles <florent.angless@gmail.com>
2017-08-16 23:18:20 +0200

    V0.1

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,4 @@
 class Picture < ApplicationRecord
 	belongs_to :event, touch: true
-	mount_uploader :image, ImageUploader
+	mount_uploader :image, ImageUploader	
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
-  
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
