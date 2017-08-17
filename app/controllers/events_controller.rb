bfcbb0bf34f62163b960dfb2868ab3c9e6f3dac4
floangles <florent.angless@gmail.com>
2017-08-17 15:14:35 +0200

    delete button + reorganisation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,6 +6,11 @@ class  EventsController < ApplicationController
 
   def show
   	event = Event.find(params[:id])  	
-  	@video = event.pictures.select{|a| File.extname(a.image_url) != '.jpg'}.first
+  	@video = Picture.first
+  end
+
+  def destroy
+  	Event.find(params[:id]).destroy
+  	redirect_to events_path
   end
 end
